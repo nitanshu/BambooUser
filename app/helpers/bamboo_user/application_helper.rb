f3d17063a7a074c8f0f518844f049bb413c29659
Praveen Kumar Sinha <praveen@headerlabs.com>
2014-10-01 23:12:51 +0530

    Adding helpers login_form, reset_password_form, login_snippet and reset_password_snippet

--- app/helpers/bamboo_user/application_helper.rb
+++ app/helpers/bamboo_user/application_helper.rb
@@ -1,8 +1,23 @@
 module BambooUser
   module ApplicationHelper
+
+    def login_form(options={}, &block)
+      form_for(:user, options.merge(url: bamboo_user.login_path), &block)
+    end
+
+    def reset_password_form(options={}, &block)
+      form_for(@user, options.merge(url: bamboo_user.reset_password_path), &block)
+    end
+
     def login_snippet(options={})
-      _default_options = {show_forgot_password: true, show_remember_me: true}
+      _default_options = {show_label: true, show_forgot_password: true, show_remember_me: true}
       render(partial: 'bamboo_user/sessions/login_form', locals: _default_options.merge(options))
     end
+
+    def reset_password_snippet(options={})
+      _default_options = {show_label: true, instructions: true, instruction_message: 'Please enter your registered email address below and click “Reset password”. You will receive an email containing instructions and a temporary link for resetting your password.'}
+      render(partial: 'bamboo_user/sessions/reset_password_form', locals: _default_options.merge(options))
+    end
+
   end
 end
