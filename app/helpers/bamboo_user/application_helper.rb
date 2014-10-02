4d6544f54b20bd341f7e62db1e19187e41595ca7
Praveen Kumar Sinha <praveen@headerlabs.com>
2014-10-02 17:10:39 +0530

    Accommodating sign-up helpers

--- app/helpers/bamboo_user/application_helper.rb
+++ app/helpers/bamboo_user/application_helper.rb
@@ -5,6 +5,10 @@ module BambooUser
       form_for(:user, options.merge(url: bamboo_user.login_path), &block)
     end
 
+    def signup_form(options={}, &block)
+      form_for(@user, options.merge(url: bamboo_user.sign_up_path), &block)
+    end
+
     def reset_password_form(options={}, &block)
       form_for(@user, options.merge(url: bamboo_user.reset_password_path), &block)
     end
@@ -16,6 +20,13 @@ module BambooUser
           show_remember_me: true,
           show_signup_link: true}
       render(partial: 'bamboo_user/sessions/login_form', locals: _default_options.merge(options))
+    end
+
+    def signup_snippet(options={})
+      _default_options = {
+          show_label: true,
+          show_extended: true}
+      render(partial: 'bamboo_user/users/signup_form', locals: _default_options.merge(options))
     end
 
     def reset_password_snippet(options={})
